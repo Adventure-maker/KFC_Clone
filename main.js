faf8b49d69b2e78ac0040bd111320e85b951ebee
Pyami <1pyami@gmail.com>
2021-05-03 07:57:09 +0530

    working

--- main.js
+++ main.js
@@ -75,8 +75,8 @@ function logIn() {
     modal2.style.display = "block";
 }
 
-var signInForm= document.getElementById('signInForm')
-var  usersName = document.getElementById('usersName')
+var signInForm = document.getElementById('signInForm')
+var usersName = document.getElementById('usersName')
 
 signInForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -84,7 +84,7 @@ signInForm.addEventListener('submit', (e) => {
     let email = document.getElementById('emailInput').value;
     let password = document.getElementById('passwordInput').value;
     let signInmessage = document.getElementById('signInMessage');
-    
+
     for (let x of users) {
         if (x.email === email && x.password === password) {
             signInmessage.setAttribute('class', 'text-success')
@@ -102,7 +102,7 @@ signInForm.addEventListener('submit', (e) => {
         else {
             signInmessage.setAttribute('class', 'text-danger')
             signInmessage.innerHTML = "Wrong password or email."
-        }   
+        }
     }
 })
 
@@ -138,22 +138,144 @@ function setLocation() {
 // Initialize and add the map
 function initMap() {
 
-    var latt; 
-    var long; 
+    var latt;
+    var long;
     navigator.geolocation.getCurrentPosition(position => {
         latt = position.coords.latitude;
         long = position.coords.longitude;
         // The location of Uluru
-    const city = { lat: latt, lng: long };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: city,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: city,
-      map: map,
-    });
+        const city = { lat: latt, lng: long };
+        // The map, centered at Uluru
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 4,
+            center: city,
+        });
+        // The marker, positioned at Uluru
+        const marker = new google.maps.Marker({
+            position: city,
+            map: map,
+        });
     })
-  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const modal4 = document.getElementById('modal4')
+function order() {
+    modal4.style.display = "block"
+}
+
+var closeBtn4 = document.getElementById('closeBtn4')
+closeBtn4.addEventListener('click', () => {
+    modal4.style.display = 'none'
+})
+
+const savedAddress = document.getElementById('place')
+const newAddress = document.getElementById('newAddress')
+savedAddress.addEventListener('change', () => {
+    if (savedAddress.checked = true) {
+        savedAddress.checked = true;
+        newAddress.checked = false;
+    } else {
+        savedAddress.checked = true;
+    }
+})
+
+newAddress.addEventListener('change', () => {
+    if (savedAddress.checked = true) {
+        savedAddress.checked = false;
+        newAddress.checked = true;
+    } else {
+        newAddress.checked = true;
+    }
+})
+
+const promoCode = document.getElementById('code')
+const promoCodeCont = document.getElementById('promoCodeCont')
+
+promoCode.addEventListener('change', () => {
+    if (promoCode.checked == false) {
+        promoCodeCont.style.display = 'none';
+    } else {
+        promoCodeCont.style.display = 'block'
+    }
+})
+
+
+const giftMessageInput = document.getElementById('giftMessageLabel')
+const textArea = document.getElementById('gift')
+
+giftMessageInput.addEventListener('change', () => {
+    if (giftMessageInput.checked == false) {
+        textArea.style.display = 'none';
+    } else {
+        textArea.style.display = 'block'
+    }
+})
+
+const payment1 = document.getElementById('creditCard')
+const payment2 = document.getElementById('paypal')
+
+payment1.addEventListener('change', () => {
+    if (payment1.checked = true) {
+        payment1.checked = true;
+        payment2.checked = false;
+    } else {
+        payment1.checked = true;
+    }
+})
+
+payment2.addEventListener('change', () => {
+    if (payment1.checked = true) {
+        payment1.checked = false;
+        payment2.checked = true;
+    } else {
+        payment2.checked = true;
+    }
+})
+
+const placeOrder = document.getElementById('placeOrder')
+const alert = document.getElementById('alert-box');
+
+placeOrder.addEventListener('click', () => {
+    alert.style.display = 'inline';
+})
